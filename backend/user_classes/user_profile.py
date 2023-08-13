74b7205fc702e8d859e07e7a7f74c336a00e7db0
DragonSasha <51650290+DragonSasha@users.noreply.github.com>
2023-08-13 03:04:17 +0300

    renamed to fit better with functionality

---
+++ backend/user_classes/user_profile.py
@@ -0,0 +1,8 @@
+from typing import List
+
+from backend.user_classes.stat import Stat
+from backend.user_classes.taks import Taks
+
+class Profile:
+    def __init__(self, stat_list:List[Stat], ) -> None:
+        pass
